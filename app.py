9aa647c73962bacf65e1c451a8a8c818f2055cec
PedroEdRoel <pedroedroel@gmail.com>
2024-10-24 09:49:24 -0300

    Company Registering

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, session, request
-import mysql.connector
+from mysql.connector import Error
 from config import *
 from db_functions import *
 
@@ -7,10 +7,13 @@ app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
 # HOME PAGE
+
 @app.route('/')
 def index():
 
     return render_template('index.html')
+
+# LOGIN PAGE
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -30,6 +33,7 @@ def login():
             session['adm'] = True
             return redirect('/admin')
 
+# ADMIN PAGE
 
 @app.route('/admin')
 def admin():
@@ -56,10 +60,49 @@ def admin():
     finally:
         DB.stop(connection, cursor)
 
+# LOGOUT ROUTE
+
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect('/')
+
+# COMPANY REGISTERING PAGE
+@app.route('/new-company', methods=['GET', 'POST'])
+def new_company ():
+    if session.get('adm') != True:
+        return redirect('/login')
+
+    elif request.method == 'GET':
+        return render_template('new-company.html')
+        
+    elif request.method == 'POST':
+        
+        companyName = request.form['name']
+        companyEmail = request.form['email']
+        companyCNPJ = request.form['cnpj']
+        companyPhoneNumber = request.form['phone']
+        companyPassword = request.form['password']
+
+        if not companyName or not companyEmail or not companyCNPJ or not companyPhoneNumber or not companyPassword:
+            return render_template('new-company.html', errormsg='All fields are obrigatory!')
+        
+        try:
+            connection, cursor = DB.connect()
+
+            SQLstatement = '''
+            INSERT INTO company VALUES
+            (null, %s, %s, %s, %s, %s, 'active') ;'''
+
+            cursor.execute(SQLstatement, (companyName, companyCNPJ, companyPhoneNumber, companyEmail, companyPassword))
+            connection.commit()
+
+        except Exception as e:
+            print(f'Error: {e}')
+        finally:
+            DB.stop(connection, cursor)
+        
+        return redirect('/admin')
 
 if environment == 'development':
     if __name__ == '__main__':
